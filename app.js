9aea987a0cf370894f5457e4346f183e7184d2cd
John <johnshew@twitter.com>
2015-11-29 13:27:06 -0800

    Initial Layout

---
+++ app.js
@@ -0,0 +1,13 @@
+var AppModule;
+(function (AppModule) {
+    var App = (function () {
+        function App() {
+            this.initialized = 0;
+            initialized = 1;
+        }
+        return App;
+    })();
+    AppModule.App = App;
+    var myApp = new App();
+})(AppModule || (AppModule = {}));
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-﻿
+﻿module AppModule {
+
+    export class App {
+        public initialized = 0;
+        constructor() {
+            initialized = 1;
+        }
+    }
+
+    var myApp = new App();
+
+}
