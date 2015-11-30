7edd2f57781c187e1e970b8057d530a6638e7591
John <johnshew@twitter.com>
2015-11-29 22:56:02 -0800

    Mostly working.

--- app.js
+++ app.js
@@ -1,13 +1,64 @@
-var AppModule;
-(function (AppModule) {
+var GraphDashboard;
+(function (GraphDashboard) {
     var App = (function () {
         function App() {
-            this.initialized = 0;
-            initialized = 1;
+            this.initialized = false;
+            this.identity = new Kurve.Identity("b8dd3290-662a-4f91-92b9-3e70fbabe04e", window.location.protocol + "//" + window.location.host
+                + window.location.pathname.substr(0, window.location.pathname.lastIndexOf("/") + 1)
+                + "login.html");
+            this.graph = new Kurve.Graph({ identity: this.identity });
+            this.initialized = true;
         }
+        App.prototype.loginAsync = function () {
+            return this.identity.loginAsync();
+        };
         return App;
     })();
-    AppModule.App = App;
-    var myApp = new App();
-})(AppModule || (AppModule = {}));
+    GraphDashboard.App = App;
+    var Controller = (function () {
+        function Controller(app) {
+            var _this = this;
+            this.app = app;
+            $('#LoginToggle').click(function (e) {
+                _this.ToggleLogin();
+            });
+        }
+        Controller.prototype.ToggleLogin = function () {
+            var _this = this;
+            if ($('#LoginState').text() == "Login") {
+                this.app.loginAsync().then(function () {
+                    if (myApp.identity.isLoggedIn()) {
+                        $('#LoginState').text("Logout");
+                        $('#Username').text(myApp.identity.getIdToken()["name"]);
+                        _this.LoadData();
+                    }
+                });
+            }
+            else {
+                this.app.identity.logOut();
+                $('#LoginState').text("Login");
+            }
+        };
+        Controller.prototype.LoadData = function () {
+            // Assumes you are logged in.
+            this.app.graph.meAsync()
+                .done(function (user) {
+                user.messagesAsync("$top=2").then(function (messageResults) {
+                    var list = $('#MailList');
+                    var messages = messageResults.resultsPage;
+                    messages.forEach(function (m) {
+                        list.append('<li class="list-group-item"><a href="#"><i class="glyphicon glyphicon-none"></i> ' + m.bodyPreview + '</a></li >');
+                    });
+                });
+            })
+                .fail(function () {
+                alert("MeAsync Failed");
+            });
+        };
+        return Controller;
+    })();
+    GraphDashboard.Controller = Controller;
+})(GraphDashboard || (GraphDashboard = {}));
+var myApp = new GraphDashboard.App();
+var myController = new GraphDashboard.Controller(myApp);
 //# sourceMappingURL=app.js.map
