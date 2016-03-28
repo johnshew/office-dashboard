772c8caeac4d4e81106a36d350c28881b929fa9c
Gon <gonzalo.aguirre@southworks.net>
2016-03-28 13:03:08 -0300

    Improve kurve type definitions import

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,4 +1,4 @@
 
 /// <reference path="react/react.d.ts" />
 /// <reference path="react/react-dom.d.ts" />
-/// <reference path="kurve.d.ts" />
+/// <reference path="../node_modules/Kurve/dist/kurve.d.ts" />
